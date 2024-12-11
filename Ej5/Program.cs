43ccae12de05145f561182141f3b5ddeb17be7c9
Xacobe <Xacobe@DESKTOP-1R86964>
2024-12-11 12:54:23 +0100

    5 coregido

--- Ej5/Program.cs
+++ Ej5/Program.cs
@@ -9,70 +9,65 @@ namespace Ej5
 {
     internal class Program
     {
-        internal class Caballo
+        static void Main(string[] args)
         {
-            static readonly private object l = new object();
-            string nombre;
-            int x;
-            int y;
-            static public bool flag = true;
-            int contador = 0;
-            public static string ganador = "";
-            static Random random = new Random();
 
-            public Caballo(string nombre, int x, int y)
+            bool flag = true;
+            while (flag)
             {
-
-                this.nombre = nombre;
-                this.x = x;
-                this.y = y;
-                posicionInicial();
-            }
-
-            public void posicionInicial()
-            {
-                Console.SetCursorPosition(x, y);
-                Console.WriteLine(nombre);
-                x = nombre.Length;
-            }
-            public void Correr()
-            {
-                while (flag)
+                int x = 0;
+                int y = 0;
+                Caballo[] caballos = new Caballo[5];
+                Thread[] hilos = new Thread[5];
+                for (int i = 0; i < caballos.Length; i++)
                 {
-                    Thread.Sleep(100);
-                    lock (l)
+                    y += 3;
+                    Caballo caballo = new Caballo("Caballo" + (i + 1), x, y);
+                    caballos[i] = caballo;
+                    Thread hilo = new Thread(caballos[i].Correr);
+                    hilos[i] = hilo;
+                }
+                Console.WriteLine("que caballo crees que va a ganar?");
+
+                String respuestaUsuario = Console.ReadLine();
+                hilos[0].Start();
+                hilos[1].Start();
+                hilos[2].Start();
+                hilos[3].Start();
+                hilos[4].Start();
+                //hilos[0].Join();
+                //hilos[1].Join();
+                //hilos[2].Join();
+                hilos[3].Join();
+                //hilos[4].Join();
+                Console.SetCursorPosition(0, 25);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("El caballo ganador es:" + Caballo.ganador);
+                Console.ResetColor();
+                if (Caballo.ganador == respuestaUsuario)
+                {
+                    Console.WriteLine("Has ganado!");
+                    Console.WriteLine("Quieres volver a jugar?(Si/No)");
+
+                    String respuesta = Console.ReadLine();
+                    if (respuesta.ToUpper() == "SI")
                     {
-                        if (flag)
-                        {
+                        Caballo.ganador = "";
+                        Caballo.flag = true;
 
-
-                            contador++;
-                            int saltos = random.Next(0, 5);
-                            x += saltos;
-                            Console.SetCursorPosition(x, y);
-                            Console.WriteLine("*");
-
-                            if (x >= 60)
-                            {
-                                flag = false;
-
-
-                                //Console.ForegroundColor = ConsoleColor.Green;
-
-                                //Console.SetCursorPosition(x, y);
-                                //Console.WriteLine("gane");
-                                ganador = nombre;
-
-
-
-                            }
-                        }
+                        Console.Clear();
+                        flag = true;
                     }
 
-
                 }
+                else
+                {
+                    Console.WriteLine("Has perdido");
+                    flag = false;
+                }
+            };
+            Console.ReadLine();
 
-            }
         }
     }
 }
